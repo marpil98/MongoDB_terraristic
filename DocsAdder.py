29aede6baf6d0d11bd5ec37422cd9b9c23b96f4b
Marcin <mar.pilewski98@gmail.com>
2025-02-10 23:00:50 +0100

    Creation class to actualization 'Stan' collection

--- DocsAdder.py
+++ DocsAdder.py
@@ -86,7 +86,7 @@ class GatunekAdder(DocsAdder):
                 # Usunięcie elementu z listy powoduje, że indeksy wszystkich następnych elementów zmniejszają się o 1
                 # Wykoanując na raz usunięcie pliku z listy i zmniejszenie o 1 wartości indeksów wszystkich plików do usunięcia,
                 # przy iterajcynym przechodzeniu w pętli przez każdy kolejny element listy to_pop (po to jest range(len), a nie po prostu lista)
-                # zapenia usunięcie elementów o odpowiednim indeksie.
+                # zapewnia usunięcie elementów o odpowiednim indeksie.
                 
                 to_pop = list(map(lambda x: x-1, to_pop))
                     
@@ -105,11 +105,24 @@ class OkazAdder(DocsAdder):
     def __init__(self, path, many=False):
         
         collection="Okazy"
+        
         super().__init__(path, collection, many)  
     
+    def _check_gat(file, existance_species=[], nonexistance_species=[], db='hodowla', uri="mongodb://localhost:27017/"):
+
+        
+        with MongoClient(uri) as client:
+            
+            gat = client[db]["Gatunek"]    
+            count = gat.count_documents({"gatunek_lac":file["gatunek_lac"]})
+            plec = file['plec']
+            stadium = file['stadium']
+                
+                
     def _update_stan(self):
         
         gatunki = []
+        
         for i in self.files:
             
             # Znaleźć nazwę gatunku w pliku
@@ -122,14 +135,81 @@ class OkazAdder(DocsAdder):
             #   stworzyć nowy stan
             pass
 
-class StanAdder(DocsAdder):
+class StanActualizer(DocsAdder):
     
-    def __init__(self, path, format, many=False):
+    def __init__(self, id_gat, count, client, file, quantity):
         
-        collection="Stan"
-        super().__init__(path, collection, format, many)
+        self.id_gat = id_gat
+        self.count = count
+        self.client = client
+        self.file = file
+        self.quantity = quantity
+        self.plec = file['płeć']
+        self.stadium = file['stadium']
+        self._actualization()
+        
+    def actualize(self):
+        
+        if self.count > 0:
+                            
+            stan = self.client['hodowla']["Stan"]  
             
-            
-adder = GatunekAdder(path="/home/marcinpielwski/MongoDB/MongoDB_terraristic/gat", many=True)
+            if self.count > 0:
+                print("Dodać aktualizację stanu")
+                # aktualizuj stan
+            else:
 
-adder.add_to_db()
+                values = self._create_vals()
+                st = Stan(values)
+                self.client['hodowla']['Stan'].insert_one(st.pola)
+                
+    def _create_vals(self):
+        
+        match self.plec:
+                
+            case "samiec":
+                
+                values = [self.id_gat, {self.stadium : self.quantity}, {}, {}]
+                
+            case "samica":
+                
+                values = [self.id_gat, {}, {self.stadium : self.quantity}, {}]
+            
+            case "nosex":
+        
+                values = [self.id_gat, {}, {}, {self.stadium : self.quantity}]
+            
+            case _:
+                
+                pass  
+            
+        return values 
+    
+    def _actualization(self):
+        
+        values = self._create_vals()
+        klucze = ["gatunek", "samce", "samice", "nosex"]
+        act_dict = dict(zip(klucze, values))
+        act_dict.pop('gatunek')
+        print(act_dict)
+        good_act_dict = {}
+        for i in act_dict:
+            
+            if len(act_dict[i]) > 0:
+                
+                for k in act_dict[i]:
+                    
+                    good_act_dict['.'.join([i,k])] = act_dict[i][k]
+        id_gat = self.id_gat
+        self.client['hodowla']['Stan'].update_one(
+            {"gatunek" : id_gat},
+            {"$inc": good_act_dict}
+        )
+        
+        
+        
+            
+stan = StanActualizer(1,2,3,{"płeć":"nosex", "stadium":"L1"},5)  
+# adder = GatunekAdder(path="/home/marcinpielwski/MongoDB/MongoDB_terraristic/gat", many=True)
+
+# adder.add_to_db()
